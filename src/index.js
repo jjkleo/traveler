b6b88cfc4c0c827f62c4c1451642119f68e590d3
jjkleo <2472650191@qq.com>
2020-12-11 20:19:04 +0800

    fontawesome配置

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "@/plugin/element.js";
+import "@/plugin/fortawesome.js";
 
 import "./style.scss";
 
